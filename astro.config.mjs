ed0e9b7027ca5a997278d0213cb489feb3ea222c
collinstewart30 <collin.stewart30@yahoo.com>
2026-07-14 14:59:00 -0400

    updated site with tailwind and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  site: 'https://giftlylists.com',
-  output: 'static',
-  integrations: [tailwind(), sitemap()]
+  site: "https://giftlylists.com",
+  output: "static",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [sitemap()],
 });
