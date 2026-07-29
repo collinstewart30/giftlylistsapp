c3168f7108f577dfecb29fe96cda12438db9ec42
collinstewart30 <collin.stewart30@yahoo.com>
2026-07-29 10:06:14 -0400

    added GA4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import partytown from "@astrojs/partytown";
+
 export default defineConfig({
   site: "https://giftlylists.com",
   output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [sitemap()],
+  integrations: [sitemap(), partytown()],
 });
